b60732d87afdc28eb3331e72850f45a759912331
Tomas Russo <trussoromera@gmail.com>
2022-09-01 19:23:32 -0300

    agregando

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './styles.scss';
+
+const NavBar = () => {
+  return (
+    <div>
+        <ul>
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">Kitesurf</a>
+            </li>
+            <li>
+                <a href="">Windsurf</a>
+            </li>
+            <li>
+                <a href="">Nosotros</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
