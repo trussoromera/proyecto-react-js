9380cec7d1d7bd19263469269466c1366ca668f1
Tomas Russo <trussoromera@gmail.com>
2022-09-11 19:50:06 -0300

    entrega map y promises

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -4,11 +4,12 @@ const Item = ({product}) => {
   return (
     <div>
         <h3>{product.nombre}</h3>
-        <img src="" alt="" />
+        
         <p>{product.precio}</p>
-        <a href=""></a>
+        
     </div>
   )
 }
+
 
 export default Item
